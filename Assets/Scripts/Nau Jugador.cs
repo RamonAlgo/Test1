4d474999981fcfce7ff73f6b49ede7c170baf10c
Ramon <ramonandres@ceroca.cat>
2023-09-18 18:45:48 +0200

    Naverminada

--- Assets/Scripts/Nau Jugador.cs
+++ Assets/Scripts/Nau Jugador.cs
@@ -4,33 +4,41 @@ using UnityEngine;
 
 public class NauJugador : MonoBehaviour
 {
-    public float _velNau;
+    public float velocidadHorizontal;
+    public float velocidadVertical;
     public float maxX;  // Límite máximo en el eje X
     public float minX; // Límite mínimo en el eje X
+    public float maxY;
+    public float minY;
 
     // Start is called before the first frame update
     void Start()
     {
-        _velNau =15f;
-        minX = -35f;
-        maxX = 35f;
+        velocidadHorizontal = 15f;
+        velocidadVertical = 10f;
+        minX = (float)-20.39f;
+        maxX = 20.39f;
+        minY = (float)-2.48f;  // Establece tus propios límites en el eje Y
+        maxY = 2.48f;   // Establece tus propios límites en el eje Y
     }
 
     // Update is called once per frame
     void Update()
     {
-        float direccioHoritzontal = Input.GetAxisRaw("Horizontal");
-        //Debug.Log(direccioHoritzontal);
-        Vector2 direccioIndicada= new Vector2 (direccioHoritzontal,0f);
+        float direccionHorizontal = Input.GetAxisRaw("Horizontal");
+        float direccionVertical = Input.GetAxisRaw("Vertical");  // Obtén la entrada vertical
 
-        Vector2 novaPos = transform.position; //Ens retorna la posicio actual de la nau
+        Vector2 direccionIndicada = new Vector2(direccionHorizontal, direccionVertical); // Utiliza ambas direcciones
 
+        Vector2 nuevaPosicion = transform.position;
 
-        novaPos += direccioIndicada * _velNau * Time.deltaTime;
+        nuevaPosicion += direccionIndicada * new Vector2(velocidadHorizontal, velocidadVertical) * Time.deltaTime;
 
-        novaPos.x = Mathf.Clamp(novaPos.x, minX, maxX);
+        // Aplica las restricciones en ambos ejes (X e Y)
+        nuevaPosicion.x = Mathf.Clamp(nuevaPosicion.x, minX, maxX);
+        nuevaPosicion.y = Mathf.Clamp(nuevaPosicion.y, minY, maxY);
 
-        transform.position = novaPos;
-        print(novaPos);
+        transform.position = nuevaPosicion;
+        print(nuevaPosicion);
     }
 }
